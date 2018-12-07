89c0ead96b53d32a79de02cfb962af64ab134323
Folio Lin <taffyzh@gmail.com>
2018-12-07 21:26:00 +0800

    Allow getting last post url

--- server.js
+++ server.js
@@ -3,12 +3,29 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const {PORT = '3000'} = process.env;
 
+let cachedPostUrl = null;
+
+const respond = (res, statusCode, contentType, body) => {
+  res.statusCode = statusCode;
+  res.setHeader('Content-Type', contentType);
+  res.end(`${body}\n`);
+};
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/xml');
-  res.end(
-    '<smsResp><code>12345</code><text>Success</text><msgid>123456789</msgid></smsResp>\n',
-  );
+  switch (req.method) {
+    case 'GET':
+      respond(res, 200, 'application/json', JSON.stringify(cachedPostUrl));
+      break;
+    case 'POST':
+      cachedPostUrl = req.url;
+      respond(
+        res,
+        200,
+        'text/xml',
+        '<smsResp><code>12345</code><text>Success</text><msgid>123456789</msgid></smsResp>',
+      );
+      break;
+  }
 });
 
 server.listen(PORT, hostname, () => {
